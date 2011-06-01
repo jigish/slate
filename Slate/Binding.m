9e7aa766c461b41d502da6a047525f5e045af2b9
Jigish Patel <jigish@ooyala.com>
2011-05-31 22:51:30 -0700

    Added license

--- Slate/Binding.m
+++ Slate/Binding.m
@@ -3,8 +3,20 @@
 //  Slate
 //
 //  Created by Jigish Patel on 5/18/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Copyright 2011 Jigish Patel. All rights reserved.
 //
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see http://www.gnu.org/licenses
 
 #import "Binding.h"
 #import "Constants.h"
