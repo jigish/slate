de4d5d32ff0b66eda5f3c0e8af4f91b85a8344df
Jigish Patel <jigish@ooyala.com>
2012-02-28 15:10:12 -0800

    update copyright

---
+++ Slate/SnapshotOperation.h
@@ -0,0 +1,14 @@
+/**
+ * @class      SnapshotOperation SnapshotOperation.h "SnapshotOperation.h"
+ * @brief      SnapshotOperation
+ * @details    SnapshotOperation.h in Slate
+ * @author     Jigish Patel
+ * @date       2/28/12
+ * @copyright  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+ */
+
+#import "Operation.h"
+
+@interface SnapshotOperation : Operation
+
+@end
---
+++ Slate/SnapshotOperation.m
@@ -0,0 +1,14 @@
+/**
+ * @file       SnapshotOperation.m
+ * @brief      Implementation of SnapshotOperation
+ * @details    SnapshotOperation.m in Slate
+ * @author     Jigish Patel
+ * @date       2/28/12
+ * @copyright  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+ */
+
+#import "SnapshotOperation.h"
+
+@implementation SnapshotOperation
+
+@end
