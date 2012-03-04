49a193e4b46b9122403b6b3baa4209c050f16644
Jigish Patel <jigish@ooyala.com>
2012-03-04 01:13:38 -0800

    update header comments

--- Slate/NSColor+Conversions.h
+++ Slate/NSColor+Conversions.h
@@ -3,22 +3,28 @@
 //  Slate
 //
 //  Created by Jigish Patel on 3/3/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright 2012 Jigish Patel. All rights reserved.
 //
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see http://www.gnu.org/licenses
 
 #import <Cocoa/Cocoa.h>
 #import <QuartzCore/QuartzCore.h>
 
 @interface NSColor (Conversions)
 
-/**
- * Return CGColor representation of the NSColor in the RGB color space
- */
 @property (readonly) CGColorRef cgColor;
 
-/**
- * Create new NSColor from a CGColorRef
- */
 + (NSColor*)colorWithCGColor:(CGColorRef)aColor;
 
 @end
--- Slate/NSColor+Conversions.m
+++ Slate/NSColor+Conversions.m
@@ -1,17 +1,28 @@
 //
-//  NSColor+Conversions.m
+//  NSColor+Conversions.h
 //  Slate
 //
 //  Created by Jigish Patel on 3/3/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright 2012 Jigish Patel. All rights reserved.
 //
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see http://www.gnu.org/licenses
 
 #import "NSColor+Conversions.h"
 
 @implementation NSColor (Conversions)
 
-- (CGColorRef)cgColor
-{
+- (CGColorRef)cgColor {
   NSColor *colorRGB = [self colorUsingColorSpaceName:NSCalibratedRGBColorSpace];
   CGFloat components[4];
   [colorRGB getRed:&components[0] green:&components[1] blue:&components[2] alpha:&components[3]];
@@ -21,8 +32,7 @@
   return theColor;
 }
 
-+ (NSColor *)colorWithCGColor:(CGColorRef)aColor
-{
++ (NSColor *)colorWithCGColor:(CGColorRef)aColor {
   const CGFloat *components = CGColorGetComponents(aColor);
   CGFloat red = components[0];
   CGFloat green = components[1];
