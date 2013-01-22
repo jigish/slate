d28fdc0f91f88fca4ea10cdf545cb41d2cf5479e
Jigish Patel <jigish@ooyala.com>
2013-01-21 17:04:25 -0800

    remove useless stuff

--- Slate/ExpressionPoint.m
+++ Slate/ExpressionPoint.m
@@ -19,7 +19,6 @@
 //  along with this program.  If not, see http://www.gnu.org/licenses
 
 #import "ExpressionPoint.h"
-#import "ScriptingController.h"
 
 
 @implementation ExpressionPoint
@@ -56,38 +55,14 @@
   return NSMakeSize([ExpressionPoint expToFloat:x withDict:values],[ExpressionPoint expToFloat:y withDict:values]);
 }
 
-+ (NSDictionary *)evaluateJS:(NSString *)exp {
-  NSMutableDictionary *dict = [NSMutableDictionary dictionary];
-  NSError *error = NULL;
-  NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"javascript\\d+"
-                                                                         options:NSRegularExpressionCaseInsensitive
-                                                                           error:&error];
-  [regex enumerateMatchesInString:exp options:0 range:NSMakeRange(0, [exp length]) usingBlock:^(NSTextCheckingResult *match, NSMatchingFlags flags, BOOL *stop){
-    for (NSUInteger i = 0; i < [match numberOfRanges]; i++) {
-      NSRange r = [match rangeAtIndex:0];
-      NSString *jsExp = [exp substringWithRange:r];
-      NSString *key = [jsExp stringByReplacingOccurrencesOfString:@"javascript" withString:@""];
-      id val = [[ScriptingController getInstance] runCallableFunction:key];
-      [dict setObject:[NSString stringWithFormat:@"%@", val] forKey:jsExp];
-    }
-  }];
-  return dict;
-}
-
 + (float)expToFloat:(NSString *)exp withDict:(NSDictionary *)values {
   if (exp != nil) {
-    // first check if there is any javascript we need to run
-    NSDictionary *jsValues = [ExpressionPoint evaluateJS:exp];
-    NSString *jsExp = [NSString stringWithString:exp];
-    for (NSString *key in [jsValues allKeys]) {
-      jsExp = [jsExp stringByReplacingOccurrencesOfString:key withString:[jsValues objectForKey:key]];
-    }
     // then do the normal predicate stuff
-    NSComparisonPredicate *pred = (NSComparisonPredicate *)[NSPredicate predicateWithFormat:[jsExp stringByAppendingString:@" == 42"]];
+    NSComparisonPredicate *pred = (NSComparisonPredicate *)[NSPredicate predicateWithFormat:[exp stringByAppendingString:@" == 42"]];
     NSExpression *lexp = [pred leftExpression];
     NSNumber *result = [lexp expressionValueWithObject:values context:nil];
     if (result == nil)
-      @throw([NSException exceptionWithName:@"Unable to compute result" reason:jsExp userInfo:nil]);
+      @throw([NSException exceptionWithName:@"Unable to compute result" reason:exp userInfo:nil]);
     return [result floatValue];
   }
   @throw([NSException exceptionWithName:@"Expression is nil" reason:exp userInfo:nil]);
